c70b7e68aafeafd544a079ad1bfaa5acf8034e48
tylim <limteikyee@gmail.com>
2021-10-14 19:11:37 +0800

    add type test

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,19 +1,19 @@
 import 'jest'
 import { objExact } from './index'
+import { expectType } from 'tsd'
+const a = objExact({ a: 1, b: 2 }, { a: 'hello', b: false as const, c: '!' })
 
+const b = objExact({ a: 1, b: undefined }, { a: 'hello', b: 123, c: '!' })
+
+const c = objExact({ a: 1, b: undefined }, { a: 'hello', c: '!' })
 describe('objSwap', () => {
 	it('test with {a:"1",b:"2",c:"3"}', () => {
-		const a = objExact(
-			{ a: 1, b: 2 },
-			{ a: 'hello', b: false as const, c: '!' }
-		)
-
-		const b = objExact({ a: 1, b: undefined }, { a: 'hello', b: 123, c: '!' })
-
-		const c = objExact({ a: 1, b: undefined }, { a: 'hello', c: '!' })
-
 		expect(a).toEqual({ a: 'hello', b: false })
 		expect(b).toEqual({ a: 'hello', b: 123 })
 		expect(c).toEqual({ a: 'hello' })
 	})
 })
+
+expectType<{ a: string; b: boolean }>(a)
+expectType<{ a: string; b: number }>(b)
+expectType<{ a: string }>(c)
